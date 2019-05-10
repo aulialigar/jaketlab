1aefd612d5ddebb7c26f3ec628413ca27f6c71b5
Putrisrsh <47842904+Putrisrsh@users.noreply.github.com>
2019-05-10 16:28:22 +0700

    Update yang kehapus

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -11,8 +11,48 @@ class User extends CI_Controller
 
 	public function index()
 	{
-		if ($this->session->)
-		// $this->data['main_view'] = 'user/v_index';
-		// $this->load->view('user/v_template',$this->data);
+		if ($this->session->userdata('logged_in') == TRUE && $this->session->userdata('userlevel') == 'mahasiswa') {
+			$this->data['tampilan_user'] = 'user/v_dashboard';
+			$this->load->view('user/v_template', $this->data);
+		}else{
+			redirect('user/login');
+		}	
 	}
+
+	public function login()
+	{
+		if ($this->session->userdata('logged_in') == TRUE) {
+			redirect('user');
+		}else{
+			if ($this->input->post('submit')) {
+				$this->form_validation->set_rules('username', 'Username', 'trim|required');
+				$this->form_validation->set_rules('password', 'Password', 'trim|required');
+
+				if ($this->form_validation->run() == TRUE)
+					{
+						if ($this->m_user->user_cek() == TRUE){
+							redirect('user');
+						} else {
+							$data['notif'] = 'Login gagal';
+							$this->load->view('user/v_login', $data);
+						}
+					// jika sukses
+
+				} else {
+					// jika gagal
+					$data['notif'] = validation_errors();
+					$this->load->view('user/v_login', $data);
+				}
+			}else{
+				$this->load->view('user/v_login');
+			}
+		}
+	}
+
+	public function logout()
+	{
+		$this->session->sess_destroy();
+		redirect('user');
+	}
+
 }
