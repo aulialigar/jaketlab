981163b506fb073d5eb31237a13f35073adee214
fachryfathurahman <fachry123fathurahman@gmail.com>
2019-05-12 06:00:48 +0700

    v.0.2.01

---
+++ application/views/user/v_form.php
@@ -0,0 +1,70 @@
+ <section>
+	<div class="container">	
+		
+			<div class="col-sm-9 col-md-6 col-lg-5 mx-auto col-md-push-3" style="border: 3px solid grey; margin-bottom:20px;">	
+				<div class="text-center" style="margin-bottom:20px;"><h2><span class="label label-info">Formulir Pemesanan Jaket Lab</span></h2> </div>
+				<form>
+  					<div class="form-group">
+					  <i class="fa fa-user"></i>
+  					  <label for="Nama">| Nama:</label>
+  						  <input type="text" class="form-control" id="Nama" placeholder="Nama">
+ 					 </div>
+
+				    <div class="form-group">
+					<i class="fa fa-id-card"></i>
+  					  <label for="Nim">| Nim:</label>
+  						  <input type="text" class="form-control" id="Nim" placeholder="Nim">
+  					</div>
+
+					  <div class="form-group">
+					  <i class="fa fa-map-marker"></i>
+  					  <label for="Alamat">| Alamat:</label>
+  						  <input type="text" class="form-control" id="Alamat" placeholder="Alamat">
+  					</div>
+
+					  <div class="form-group">
+					  <i class="fa fa-phone"></i>
+  					  <label for="Nomor">| No. telp:</label>
+  						  <input type="text" class="form-control" id="Alamat" placeholder="no.telp">
+  					</div>
+					
+					<div class="form-group">
+					<i class="fa fa-shirtsinbulk"></i>
+						<label for="ukuran">| Ukuran Baju:</label>
+					</div>
+
+
+  
+    				<div class="form-group">
+      					<select class="form-control">
+							<option value="pilih">pilih</option>
+        					<option value="XL">XL</option>
+        					<option value="L">L</option>
+        					<option value="M">M</option>
+							<option value="S">S</option>
+      					</select>
+    				</div>
+ 
+ 
+					<div class="form-group">
+					<p>
+					<i class="fa fa-upload"></i>
+					| Upload bukti pembayaran
+					</p>
+						<div class="custom-file">
+  							<div class="input-group-addon">
+    							<input type="file" class="custom-file-input" id="bukti pembayaran"
+    								  aria-describedby="inputGroupFileAddon01">
+    								
+ 							 </div>
+						</div>
+					</div>
+
+
+
+  						<button type="submit" class="btn btn-primary pull-right" style="margin-bottom:20px; margin-top:20px;">Pesan</button>
+				</form>
+				
+		</div>	
+	</div>
+</section>
