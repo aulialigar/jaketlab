300920ca82d26f29f8b8e717f979ae2048bbf72b
fachryfathurahman <fachry123fathurahman@gmail.com>
2019-05-11 11:10:20 +0700

    v.0.1.02

--- application/views/user/v_template.php
+++ application/views/user/v_template.php
@@ -296,13 +296,13 @@
 						<div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
 							<div class="layer-slick1 animated visible-false" data-appear="fadeInDown" data-delay="0">
 								<span class="ltext-101 cl2 respon2">
-									Women Collection 2018
+									Pemesanan
 								</span>
 							</div>
 								
 							<div class="layer-slick1 animated visible-false" data-appear="fadeInUp" data-delay="800">
 								<h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
-									NEW SEASON
+									Jaket Laboratorium
 								</h2>
 							</div>
 								
@@ -421,65 +421,46 @@
 	<footer class="bg3 p-t-75 p-b-32">
 		<div class="container">
 			<div class="row">
-				<div class="col-sm-6 col-lg-3 p-b-50">
+			
+				<div class="col-sm-6 col-lg-6 p-b-50">
 					<h4 class="stext-301 cl0 p-b-30">
-						Categories
+						Contact Us
 					</h4>
 
 					<ul>
 						<li class="p-b-10">
+							<a href="#" class="stext-107 hov-cl1 trans-04" color: green>
+							Address: 
+							</a>
 							<a href="#" class="stext-107 cl7 hov-cl1 trans-04">
-								Women
+							Jl. Raya Tlogomas No.246, Jawa Timur 65144, Indonesia
 							</a>
 						</li>
 
 						<li class="p-b-10">
+						<a href="#" class="stext-107 hov-cl1 trans-04" color: green>
+						Phone: 
+							</a>
 							<a href="#" class="stext-107 cl7 hov-cl1 trans-04">
-								Men
+							(0341) 464318, ext 252 - SMS/WA/Telegram <br />(Quick Response) +62 896-896-01317 
 							</a>
 						</li>
 
 						<li class="p-b-10">
+						<a href="#" class="stext-107 hov-cl1 trans-04" color: green>
+						email1: 
+							</a>
 							<a href="#" class="stext-107 cl7 hov-cl1 trans-04">
-								Shoes
+							lab.informatika@umm.ac.id 
 							</a>
 						</li>
 
 						<li class="p-b-10">
-							<a href="#" class="stext-107 cl7 hov-cl1 trans-04">
-								Watches
+						<a href="#" class="stext-107 hov-cl1 trans-04" color: green>
+						email2: 
 							</a>
-						</li>
-					</ul>
-				</div>
-
-				<div class="col-sm-6 col-lg-3 p-b-50">
-					<h4 class="stext-301 cl0 p-b-30">
-						Help
-					</h4>
-
-					<ul>
-						<li class="p-b-10">
 							<a href="#" class="stext-107 cl7 hov-cl1 trans-04">
-								Track Order
-							</a>
-						</li>
-
-						<li class="p-b-10">
-							<a href="#" class="stext-107 cl7 hov-cl1 trans-04">
-								Returns 
-							</a>
-						</li>
-
-						<li class="p-b-10">
-							<a href="#" class="stext-107 cl7 hov-cl1 trans-04">
-								Shipping
-							</a>
-						</li>
-
-						<li class="p-b-10">
-							<a href="#" class="stext-107 cl7 hov-cl1 trans-04">
-								FAQs
+							 lab.informatika.umm@gmail.com 
 							</a>
 						</li>
 					</ul>
@@ -491,7 +472,7 @@
 					</h4>
 
 					<p class="stext-107 cl7 size-201">
-						Any questions? Let us know in store at 8th floor, 379 Hudson St, New York, NY 10018 or call us on (+1) 96 716 6879
+						Ada pertanyaan? silahkan datang ke kantor laboratorium informatika yang berada di GKB 3 lantai 6 di Universitas Muhammadiyah Malang
 					</p>
 
 					<div class="p-t-27">
@@ -529,36 +510,7 @@
 				</div>
 			</div>
 
-			<div class="p-t-40">
-				<div class="flex-c-m flex-w p-b-18">
-					<a href="#" class="m-all-1">
-						<img src="<?php echo base_url(); ?>assets/images/icons/icon-pay-01.png" alt="ICON-PAY">
-					</a>
-
-					<a href="#" class="m-all-1">
-						<img src="<?php echo base_url(); ?>assets/images/icons/icon-pay-02.png" alt="ICON-PAY">
-					</a>
-
-					<a href="#" class="m-all-1">
-						<img src="<?php echo base_url(); ?>assets/images/icons/icon-pay-03.png" alt="ICON-PAY">
-					</a>
-
-					<a href="#" class="m-all-1">
-						<img src="<?php echo base_url(); ?>assets/images/icons/icon-pay-04.png" alt="ICON-PAY">
-					</a>
-
-					<a href="#" class="m-all-1">
-						<img src="<?php echo base_url(); ?>assets/images/icons/icon-pay-05.png" alt="ICON-PAY">
-					</a>
-				</div>
-
-				<p class="stext-107 cl6 txt-center">
-					<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-
-				</p>
-			</div>
+			
 		</div>
 	</footer>
 
